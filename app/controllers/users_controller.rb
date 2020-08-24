3995f6641b80adaab853bfdff19b52f7182b127b
Marina Rozas <rozasmarina@gmail.com>
2020-08-24 18:08:19 -0300

    default private functions to user controller and new function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,16 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: %i[show create]
+  def new
+    @user = User.new
+  end
+
+  private
+
+  def users_params
+    params.require(:user).permit(:first_name, :last_name, :address, :phone, :username, :email, :cpf)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
+  end
 end

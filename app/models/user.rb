6fba6321daac5e72494bfed774cf950c8fffbc1c
Marina Rozas <rozasmarina@gmail.com>
2020-08-24 15:45:03 -0300

    added validations to model User

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :announces
+  has_many :transactions
+  validates :first_name, :last_name, :address, :phone, :username, :email, :cpf, presence: true
+  validates :username, :email, :cpf, uniqueness: true
 end

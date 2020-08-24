20254ad4eee004267fe6d4e1cfd06f85bb2784f6
Deborah Carvalho <deborah.bpc@gmail.com>
2020-08-24 16:28:11 -0300

    adding some validations. still thinking on some more

--- app/models/announce.rb
+++ app/models/announce.rb
@@ -1,4 +1,9 @@
 class Announce < ApplicationRecord
   belongs_to :user
   has_many :transactions
+
+  validates :price, :quantity, :product_name, :product_description, :product_category, presence: true
+  validates :product_description, length: { minimum: 50 }
+  validates :price, :quantity, numericality: true
+  validates :price, :quantity, numericality: { greater_than: 0 }
 end
--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,5 +1,8 @@
 class Transaction < ApplicationRecord
   belongs_to :announce
   belongs_to :user
+
   validates :price, :quantity, presence: true
+  validates :price, :quantity, numericality: true
+  validates :price, :quantity, numericality: { greater_than: 0 }
 end

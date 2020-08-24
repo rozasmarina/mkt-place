60349ca279344a858d7ed7826a9af5a8b2b05133
Deborah Carvalho <deborah.bpc@gmail.com>
2020-08-24 16:39:48 -0300

    Added active attribute to announce. Default: true

--- app/models/announce.rb
+++ app/models/announce.rb
@@ -2,8 +2,8 @@ class Announce < ApplicationRecord
   belongs_to :user
   has_many :transactions
 
-  validates :price, :quantity, :product_name, :product_description, :product_category, presence: true
-  validates :product_description, length: { minimum: 50 }
+  validates :price, :quantity, :product_name, :product_description, :product_category, :active, presence: true
+  validates :product_description, length: { minimum: 100 }
   validates :price, :quantity, numericality: true
   validates :price, :quantity, numericality: { greater_than: 0 }
 end

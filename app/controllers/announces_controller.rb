6706e8d9c8d5f4e6076f241285675d029706c324
Deborah Carvalho <deborah.bpc@gmail.com>
2020-08-25 10:43:19 -0300

    Setting actions to announces controller. Still need strong params

--- app/controllers/announces_controller.rb
+++ app/controllers/announces_controller.rb
@@ -1,6 +1,31 @@
 class AnnouncesController < ApplicationController
+  before_action :set_announce, only: [:show, :edit, :update, :destroy]
 
   def index
     @announces = Announce.all
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def set_announce
+    @announce = Announce.find(params[:id])
   end
 end

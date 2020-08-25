8a5e6257ce6c4bda1f958359f0a78eb828e68eda
Marina Rozas <rozasmarina@gmail.com>
2020-08-25 12:41:23 -0300

    routes for announces and users OK. orders missing

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   root to: 'announces#index'
+  resources :announces, only: [:show, :edit, :update, :destroy]
   devise_for :users
-  resources :announces, except: [:index]
+  resources :users do
+    resources :announces, only: [:new, :create]
+  end
+  
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

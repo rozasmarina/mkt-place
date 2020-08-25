e26bf97958c22c5dd8c8e775cd28ffa94c26f9b9
Ed <ed.colen@gmail.com>
2020-08-25 19:45:47 -0300

    starting modal on announces show

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -8,11 +8,17 @@ class OrdersController < ApplicationController
   end
 
   def create
-    @order = Order.new(cocktail_params)
+    @order = Order.new(orders_params)
     if @order.save
       redirect_to @order
     else
       render :new
     end
+  end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:price, :quantity, :announce, :user)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :users do
     resources :announces, only: %i[new create]
-    resources :orders, only: %i[index create show]
+    resources :orders, only: %i[index new create show]
   end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html

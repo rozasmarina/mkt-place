c2a3bec2d2e445716b6543286cf5ef76f457cfc3
Ed <ed.colen@gmail.com>
2020-08-25 12:20:27 -0300

    added photo attachments to user and announce models

--- app/models/announce.rb
+++ app/models/announce.rb
@@ -1,6 +1,7 @@
 class Announce < ApplicationRecord
   belongs_to :user
   has_many :orders
+  has_one_attached :photo
 
   validates :price, :quantity, :product_name, :product_description, :product_category, :active, :announce_type, presence: true
   validates :product_description, length: { minimum: 100 }
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :announces
   has_many :order
+  has_one_attached :photo
   validates :first_name, :last_name, :address, :phone_number, :username, :email, :cpf, presence: { message: "Campo obrigatório" }
   validates :username, :email, :cpf, uniqueness: true
   validates :email, confirmation: true

198bea40b9692168e473111944c1158cd23e8e76
Deborah Carvalho <deborah.bpc@gmail.com>
2020-09-16 16:46:23 -0300

    adding meta tags. Still need photo

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,10 @@ class ApplicationController < ActionController::Base
 
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  def default_url_options
+    { host: ENV["DOMAIN"] || "localhost:3000" }
+  end
+
   protected
 
   def configure_permitted_parameters
